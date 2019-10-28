aa40b946e06f223e06a16de0b6885e3c7aed9093
Adi George <adi_george@outlook.com>
2019-10-28 12:53:36 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -108,8 +108,9 @@ function setActiveTabNav(pageId) {
 
 //Chart
 
-const _db = firebase.firestore();
+co nst _db = firebase.firestore();
 const _dataRef = _db.collection("output-data");
+const _outputRefNorthDenmark = _db.collection("output-data").doc("year").collection("north-denmark");
 let _sustainabilityData;
 
 // listen for changes on _dataRef
@@ -120,55 +121,13 @@ _dataRef.orderBy("year").onSnapshot(function(snapshotData) {
     data.id = doc.id; // add the id to the data variable
     _sustainabilityData.push(data); // push the data object to the global array _sustainabilityData
   });
+  if (data.region === 'north-denmark') {
   console.log(_sustainabilityData);
-  appendCows(_sustainabilityData); // call appendCows with _sustainabilityData as function argument
   appendCarbonFootprint(_sustainabilityData); //call appendCarbonFootprint with _sustainabilityData as function argument
-  appendMilkProduction(_sustainabilityData); //call appendMilkProduction with _sustainabilityData as function argument
+
 });
 
-function appendCows(sustainabilityData) {
-  // prepare data
-  let cows = [];
-  let years = [];
-  sustainabilityData.forEach(data => {
-    if (data.region === 'north') {
-      cows.push(data.herdYearCows);
-      years.push(data.year);
-    }
-  });
 
-
-
-  // generate chart
-  let chart = document.querySelector('#cows');
-  let myDoughnutChart = new Chart(chart, {
-    type: 'bar',
-    data: {
-      datasets: [{
-        data: cows,
-        label: 'Number of Cows',
-        fill: false,
-        borderColor: "#e755ba",
-        backgroundColor: "#e755ba",
-        pointBackgroundColor: "#55bae7",
-        pointBorderColor: "#55bae7",
-        pointHoverBackgroundColor: "#55bae7",
-        pointHoverBorderColor: "#55bae7",
-      }],
-      labels: years
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            min: (Math.min(...cows) - 5),
-            max: (Math.max(...cows) + 1)
-          }
-        }]
-      }
-    }
-  });
-}
 
 function appendCarbonFootprint(sustainabilityData) {
   // prepare data
@@ -205,53 +164,5 @@ function appendCarbonFootprint(sustainabilityData) {
   });
 }
 
-function appendMilkProduction(sustainabilityData) {
-  let years = [];
-  let milkNorth = [];
-  let milkSouth = [];
-  sustainabilityData.forEach(data => {
-    if (data.region === 'north') {
-      milkNorth.push(data.herdMilkProduction.replace(',', '.'));
-      years.push(data.year);
-    } else if (data.region === 'south') {
-      milkSouth.push(data.herdMilkProduction.replace(',', '.'));
-    }
-  });
-
-  console.log(years);
-  console.log(milkNorth);
-  console.log(milkSouth);
-
-  // generate chart
-  let chart = document.querySelector('#milkProduction');
-  let myDoughnutChart = new Chart(chart, {
-    type: 'line',
-    data: {
-      datasets: [{
-        data: milkNorth,
-        label: 'Milk Production North',
-        fill: false,
-        borderColor: "#e755ba",
-        backgroundColor: "#e755ba",
-        pointBackgroundColor: "#55bae7",
-        pointBorderColor: "#55bae7",
-        pointHoverBackgroundColor: "#55bae7",
-        pointHoverBorderColor: "#55bae7",
-      }, {
-        label: 'Milk Production South',
-        data: milkSouth,
-        fill: false,
-        borderColor: "#55bae7",
-        backgroundColor: "#55bae7",
-        pointBackgroundColor: "#e755ba",
-        pointBorderColor: "#e755ba",
-        pointHoverBackgroundColor: "#e755ba",
-        pointHoverBorderColor: "#e755ba",
-        type: 'line'
-      }],
-      labels: years
-    }
-  });
-}
 
 //End of chart
