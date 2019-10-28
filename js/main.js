2a1a9bfe1d5611e8eaaaa13755fb3dae382187db
alexmaslarski <38736057+alexmaslarski@users.noreply.github.com>
2019-10-28 13:27:05 +0100

    j

--- js/main.js
+++ js/main.js
@@ -3,40 +3,40 @@
 
 // Navigation
 let hideAllPages = () => {
-  let pages = document.querySelectorAll(".page");
-  for (let page of pages) {
-    page.style.display = "none";
-  }
+    let pages = document.querySelectorAll(".page");
+    for (let page of pages) {
+        page.style.display = "none";
+    }
 }
 // show page or tab
 let showPage = pageId => {
-  hideAllPages();
-  document.querySelector(`#${pageId}`).style.display = "block";
-  location.href = `#${pageId}`;
-  setActiveTab(pageId);
-  // showLoader(500);
+    hideAllPages();
+    document.querySelector(`#${pageId}`).style.display = "block";
+    location.href = `#${pageId}`;
+    setActiveTab(pageId);
+    // showLoader(500);
 }
 // set default page
 let setDefaultPage = () => {
-  let page = "profile-page";
-  if (location.hash) {
-    page = location.hash.slice(1);
-  }
-  showPage(page);
+    let page = "profile-page";
+    if (location.hash) {
+        page = location.hash.slice(1);
+    }
+    showPage(page);
 }
 setDefaultPage();
 
 function setActiveTab(pageId) {
-  let pages = document.querySelectorAll("nav a");
-  for (let page of pages) {
-    if (`#${pageId}` === page.getAttribute("href")) {
-      page.classList.add("active");
-    } else {
-      page.classList.remove("active");
-    }
+    let pages = document.querySelectorAll("nav a");
+    for (let page of pages) {
+      if (`#${pageId}` === page.getAttribute("href")) {
+        page.classList.add("active");
+      } else {
+        page.classList.remove("active");
+      }
 
+    }
   }
-}
 
 /*
 let showLoader = (duration) => {
@@ -53,7 +53,7 @@ setTimeout(() => {
 var acc = document.getElementsByClassName("category");
 
 for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
+  acc[i].addEventListener("click", function() {
     // this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
@@ -67,10 +67,10 @@ for (let i = 0; i < acc.length; i++) {
 
 //SPA for the navigation inside the profile page
 let hideAllNavPages = () => {
-  let pages = document.querySelectorAll(".profileNavPage");
-  for (let page of pages) {
-    page.style.display = "none";
-  }
+    let pages = document.querySelectorAll(".profileNavPage");
+    for (let page of pages) {
+        page.style.display = "none";
+    }
 }
 // show page or tab
 function showNavPage(pageId){
@@ -100,29 +100,77 @@ const _db = firebase.firestore();
 const _dataRef = _db.collection("output-data");
 let _sustainabilityData;
 
-
-
 // listen for changes on _dataRef
-_dataRef.orderBy("year").onSnapshot(function (snapshotData) {
+_dataRef.orderBy("year").onSnapshot(function(snapshotData) {
   _sustainabilityData = []; // reset _sustainabilityData
   snapshotData.forEach(doc => { // loop through snapshotData - like for of loop
     let data = doc.data(); // save the data in a variable
-    _sustainabilityData.push(data);
+    data.id = doc.id; // add the id to the data variable
+    _sustainabilityData.push(data); // push the data object to the global array _sustainabilityData
   });
-
-
-  appendFootprint(_sustainabilityData);
-
-
+  console.log(_sustainabilityData);
+  appendCows(_sustainabilityData); // call appendCows with _sustainabilityData as function argument
+  appendCarbonFootprint(_sustainabilityData); //call appendCarbonFootprint with _sustainabilityData as function argument
+  appendMilkProduction(_sustainabilityData); //call appendMilkProduction with _sustainabilityData as function argument
 });
 
-function appendFootprint(sustainabilityData) {
-  let totalCarbonFootprint = [];
+function appendCows(sustainabilityData) {
+  // prepare data
+  let cows = [];
   let years = [];
   sustainabilityData.forEach(data => {
-    totalCarbonFootprint.push(data.totalFootprint);
-    years.push(data.year);
+    if (data.region === 'north') {
+      cows.push(data.herdYearCows);
+      years.push(data.year);
+    }
   });
+
+
+
+  // generate chart
+  let chart = document.querySelector('#cows');
+  let myDoughnutChart = new Chart(chart, {
+    type: 'bar',
+    data: {
+      datasets: [{
+        data: cows,
+        label: 'Number of Cows',
+        fill: false,
+        borderColor: "#e755ba",
+        backgroundColor: "#e755ba",
+        pointBackgroundColor: "#55bae7",
+        pointBorderColor: "#55bae7",
+        pointHoverBackgroundColor: "#55bae7",
+        pointHoverBorderColor: "#55bae7",
+      }],
+      labels: years
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            min: (Math.min(...cows) - 5),
+            max: (Math.max(...cows) + 1)
+          }
+        }]
+      }
+    }
+  });
+}
+
+function appendCarbonFootprint(sustainabilityData) {
+  // prepare data
+  let carbonFootprint = [];
+  let years = [];
+  sustainabilityData.forEach(data => {
+    if (data.region === 'north-denmark') {
+      carbonFootprint.push(data.carbonFootprintWholeFarm.replace(',', '.'));
+      years.push(data.year);
+    }
+  });
+
+  console.log(carbonFootprint);
+  console.log(years);
 
   // generate chart
   let chart = document.querySelector('#carbonFootprint');
@@ -130,8 +178,8 @@ function appendFootprint(sustainabilityData) {
     type: 'line',
     data: {
       datasets: [{
-        data: totalCarbonFootprint,
-        label: 'kg CO2 per year',
+        data: total-footprint-output,
+        label: 'Carbon Footprint WholeFarm',
         fill: false,
         borderColor: "#e755ba",
         backgroundColor: "#e755ba",
@@ -143,6 +191,55 @@ function appendFootprint(sustainabilityData) {
       labels: years
     }
   });
+}
 
+function appendMilkProduction(sustainabilityData) {
+  let years = [];
+  let milkNorth = [];
+  let milkSouth = [];
+  sustainabilityData.forEach(data => {
+    if (data.region === 'north') {
+      milkNorth.push(data.herdMilkProduction.replace(',', '.'));
+      years.push(data.year);
+    } else if (data.region === 'south') {
+      milkSouth.push(data.herdMilkProduction.replace(',', '.'));
+    }
+  });
+
+  console.log(years);
+  console.log(milkNorth);
+  console.log(milkSouth);
+
+  // generate chart
+  let chart = document.querySelector('#milkProduction');
+  let myDoughnutChart = new Chart(chart, {
+    type: 'line',
+    data: {
+      datasets: [{
+        data: milkNorth,
+        label: 'Milk Production North',
+        fill: false,
+        borderColor: "#e755ba",
+        backgroundColor: "#e755ba",
+        pointBackgroundColor: "#55bae7",
+        pointBorderColor: "#55bae7",
+        pointHoverBackgroundColor: "#55bae7",
+        pointHoverBorderColor: "#55bae7",
+      }, {
+        label: 'Milk Production South',
+        data: milkSouth,
+        fill: false,
+        borderColor: "#55bae7",
+        backgroundColor: "#55bae7",
+        pointBackgroundColor: "#e755ba",
+        pointBorderColor: "#e755ba",
+        pointHoverBackgroundColor: "#e755ba",
+        pointHoverBorderColor: "#e755ba",
+        type: 'line'
+      }],
+      labels: years
+    }
+  });
+}
 
 }
